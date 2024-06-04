d0ff7d09b21952fc2b1b6918eca1b841560b9c64
Tim Goalen <timgoalen@hotmail.com>
2024-06-04 08:24:45 +0000

    chore: reset app for assemblyai streaming implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Whisper STT Experiment",
+  title: "AsseblyAI STT Experiment",
   description: "Speech to text",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,12 @@
-"use client";
+// "use client";
 
-import Image from "next/image";
 // import styles from "./page.module.css";
 
-import WhisperUI from "./components/WhisperUI";
-import WhisperAPI from "./components/WhisperAPI";
-import { useState } from "react";
-
 export default function Home() {
-  const [isRecording, setIsRecording] = useState(false);
 
   return (
     <main>
-      {/* <WhisperAPI /> */}
-      {isRecording && <WhisperUI />}
-
-      <button onClick={() => setIsRecording(true)}>Start</button>
-      <button onClick={() => setIsRecording(false)}>Stop</button>
+      <h1>AssemblyAI</h1>
     </main>
   );
 }
